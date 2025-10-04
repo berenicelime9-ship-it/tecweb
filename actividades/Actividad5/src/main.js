c4c0eb76098aa39fdbbfc621addd30f78cbee9c6
berenicelime9 <berenice.lime9@gmail.com>
2025-10-04 12:54:14 -0600

    carpeta actividades creada

---
+++ actividades/Actividad5/src/main.js
@@ -0,0 +1,18 @@
+
+function getDatos()
+{
+    var nombre = prompt("Nombre: ", "");
+
+    var edad = prompt("Edad: ", 0);
+
+    var div1 = document.getElementById('nombre');
+    div1.innerHTML = '<h3> Nombre: '+nombre+'</h3>';
+
+    var div2 = document.getElementById('edad');
+    div2.innerHTML = '<h3> Edad: '+edad+'</h3>';
+}
+
+//funcion del ejemplo 1
+function ejemplo1() {
+    document.write('Hola Mundo');
+}
